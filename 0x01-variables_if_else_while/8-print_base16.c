89ae48d2fc15cef43f4c63c5beff3f38ab1bef98
Jactone Ondidha <jactoneondidha@gmail.com>
2022-09-11 09:59:21 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int z;
+
+
+
+	for (z = 36; z <= 60; z++)
+
+	{
+
+		putchar(z);
+
+	}
+
+	for (z = 95; z <= 109;z++)
+
+	{
+
+		putchar(z)
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
