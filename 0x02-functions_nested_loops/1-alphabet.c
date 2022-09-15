bd91ad88883d2c540ba6cde14062789d40047934
Jactone Ondidha <jactoneondidha@gmail.com>
2022-09-14 21:36:55 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - check the code for Jactone.
+ * Return: Always 0.
+ */
+
+
+
+void print_alphabet(void)
+
+{
+
+		char alp = 'a';
+
+
+
+			while (alp <= 'z')
+
+					{
+
+								_putchar (alp);
+
+										alp++;
+
+
+
+											}
+
+				_putchar ('\n');
+
+
+
+}
