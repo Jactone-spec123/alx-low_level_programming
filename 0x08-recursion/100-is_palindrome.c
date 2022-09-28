dfc2326dbb50bb0392a36d24953140336439d8aa
Jactone Ondidha <jactoneondidha@gmail.com>
2022-09-28 12:16:47 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,74 +1,50 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * wildcmp - check the code for Holberton School students.
- * @s1: string
- * @s2: string
- * Return: Always 0.
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
 
-int wildcmp(char *s1, char *s2)
+int _strlen_recursion(char *s)
 {
-return (checker(s1, s2, 0, 0, -1));
-}
-
-/**
- * checker - helper
- * @s1: string
- * @s2: string
- * @a: int
- * @b: int
- * @wildUsed: int
- * Return: Always 0.
- */
-int checker(char *s1, char *s2, int a, int b, int wildUsed)
-{
-
-//printf("s1 <%c> a <%i> s2 <%c> b <%i> \n", s1[a], a, s2[b], b);
-
-if (s1[a] != '\0')
-{	
-	if (s2[b] == '\0')
+	if (!*s)
+	{
 		return (0);
-	else if (s2[b] == '*')
-	{
-		if (s2[b + 1] == '*'){
-			//printf("Pass 1\n");
-			return (checker(s1, s2, a, b + 1, b));
-		}
-		else if (s2[b + 1] == s1[a]){
-			//printf("Pass 2\n");
-			return (checker(s1, s2, a, b + 1, b));
-		}	
-		else if (s1[a + 1] != s2[b + 1]){
-			//printf("Pass 3\n");
-			return (checker(s1, s2, a + 1, b, b));	
-		}
-		else if (s1[a + 1] == s2[b + 1]){
-			//printf("Pass 3\n");
-			return (checker(s1, s2, a + 1, b + 1, b));	
-		}
-		
 	}
-	else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1])){
-		//printf("Pass 4\n");
-		return (checker(s1, s2, a + 1, b + 1, wildUsed));
-	}
-	else
-	{
-		if (wildUsed == -1)
-		{
-			//printf("No wild card\n");
-			return (0);
-		}
-		//printf("Back to wildcard\n");
-		return (checker(s1, s2, a, wildUsed, wildUsed));
-	}
+	return (1 + _strlen_recursion(++s));
 }
-if (s2[b] != '\0')
-	return 0;
 
-return (1);
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
 
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
